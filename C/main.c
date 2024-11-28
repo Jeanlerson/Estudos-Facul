fee5169f44a19efdbb4c721bb3ebe854c7d80d10
Jeanlerson Santos <jeanlersonsantossilva@gmail.com>
2024-11-27 22:02:25 -0300

    compilando

--- C/main.c
+++ C/main.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <locale.h>
 
@@ -8,6 +9,22 @@ int main() {
     scanf("%d", &x);
 
     printf("Hello World");
-    printf("\nOlá Mundo! X = %d", x);
+    printf("\nOlá Mundo! X = %d\n", x);
+
+    //
+
+    int res;
+    int b = 1;
+    int n;
+
+    scanf("%d", &n);
+    
+    while (b <= n) {
+        res = 2 * b;
+        printf("Tabuada de 2: 2 x %d = %d\n",b, res);
+
+        b++;
+    };
+
     return 0;
 }
